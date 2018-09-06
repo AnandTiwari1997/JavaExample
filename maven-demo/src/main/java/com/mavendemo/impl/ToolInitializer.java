9ad4ca91534600326b0790697a1608e9c7960302
AnandTiwari1997 <anandtiwari887@gmail.com>
2018-09-06 22:30:25 +0530

    Fourth Commit

--- maven-demo/src/main/java/com/mavendemo/impl/ToolInitializer.java
+++ maven-demo/src/main/java/com/mavendemo/impl/ToolInitializer.java
@@ -7,68 +7,72 @@ import org.apache.log4j.Logger;
 
 import com.mavendemo.exceptions.EmptyArrayListException;
 
-/** This the class that contain Main Method.
+/**
+ * This the class that contain Main Method.
  * 
- * @author anand.tiwari */
+ * @author anand.tiwari
+ */
 public class ToolInitializer {
 
-    private static Logger logger = Logger.getLogger(ToolInitializer.class);
-    private static Scanner scanner = new Scanner(System.in);
+	private static Logger logger = Logger.getLogger(ToolInitializer.class);
+	private static Scanner scanner = new Scanner(System.in);
 
-    /** Main Method Execution starts from here.
-     * 
-     * @param args
-     *            other argument. */
-    public static void main(String[] args) {
+	/**
+	 * Main Method Execution starts from here.
+	 * 
+	 * @param args
+	 *            other argument.
+	 */
+	public static void main(String[] args) {
 
-        logger.info("Starting Application.....");
+		logger.info("Starting Application.....");
 
-        // MyArrayList arrayList = new MyArrayList();
-        // try {
-        // arrayList.contains("John");
-        // } catch (EmptyArrayListException e1) {
-        // logger.error(e1.getMessage());
-        // }
-        //
-        //
-        // ObjectClass objectClass = new ObjectClass();
-        // logger.info(objectClass);
-        // logger.info("Generated hashcode is : " + objectClass.hashCode());
+		// MyArrayList arrayList = new MyArrayList();
+		// try {
+		// arrayList.contains("John");
+		// } catch (EmptyArrayListException e1) {
+		// logger.error(e1.getMessage());
+		// }
+		//
+		//
+		// ObjectClass objectClass = new ObjectClass();
+		// logger.info(objectClass);
+		// logger.info("Generated hashcode is : " + objectClass.hashCode());
 
-        String name;
-        StringBuilder builder = new StringBuilder();
-        builder.append("\n");
+		String name;
+		StringBuilder builder = new StringBuilder();
+		builder.append("\n");
 
-        if (args.length > 0)
-            name = args[0];
-        else {
-            logger.info("Enter the Package Name (e.g. java.util.random) : ");
-            name = scanner.next();
-        }
+		if (args.length > 0)
+			name = args[0];
+		else {
+			logger.info("Enter the Package Name (e.g. java.util.random) : ");
+			name = scanner.next();
+		}
 
-        try {
-            Class<?> cls = Class.forName(name);
-            Class<?> superCls = cls.getSuperclass();
+		try {
+			Class<?> cls = Class.forName(name);
+			Class<?> superCls = cls.getSuperclass();
 
-            String modifiers = Modifier.toString(cls.getModifiers());
-            if (modifiers.length() > 0)
-                builder.append(modifiers + " ");
-            builder.append(name);
-            if (superCls != null && superCls != Object.class)
-                builder.append(" extends " + superCls.getName());
-            builder.append("{\n");
-            builder.append(GetClassDetails.printConstructor(cls));
-            builder.append(GetClassDetails.printMethods(cls));
-            builder.append(GetClassDetails.printFields(cls));
-            builder.append("\n}");
+			String modifiers = Modifier.toString(cls.getModifiers());
+			if (modifiers.length() > 0)
+				builder.append(modifiers + " ");
+			builder.append(name);
+			if (superCls != null && superCls != Object.class)
+				builder.append(" extends " + superCls.getName());
+			builder.append("{\n");
+			builder.append(GetClassDetails.printConstructor(cls));
+			builder.append(GetClassDetails.printMethods(cls));
+			builder.append(GetClassDetails.printFields(cls));
+			builder.append("\n}");
 
-            logger.info(builder);
-        } catch (ClassNotFoundException e) {
-            logger.error(e.getMessage());
-        }
+			logger.info(builder);
+		} catch (ClassNotFoundException e) {
+			logger.error(e.getMessage());
+		}
 
-        logger.info("Ending Application.......");
+		logger.info("Ending Application.......");
 
-    }
+	}
 
 }
